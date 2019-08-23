060e71f89f245b46377d00f836961712619ffc3c
JessieW0010 <jessiewang@Jessies-MacBook-Pro.local>
2019-08-22 20:52:19 -0400

    finished component set up

---
+++ components/Key.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+
+export default function Key(props) {
+	function onPress() {
+		// code later
+	}
+
+	return (
+		<TouchableHighlight
+			onPress={onPress}
+			style={props.op ? [styles.opkey, styles.key] : styles.key}
+		>
+			<View>
+				<Text style={props.op ? styles.opKeyText : styles.keyText}>
+					{props.symbol}
+				</Text>
+			</View>
+		</TouchableHighlight>
+	);
+}
+
+const styles = StyleSheet.create({
+	key: {
+		borderRadius: 30,
+		backgroundColor: "#c7c7c7",
+		width: 60,
+		height: 60,
+		justifyContent: "center",
+		alignItems: "center"
+	},
+	keyText: {
+		fontSize: 20
+	},
+	opkey: {
+		backgroundColor: "#989898",
+		marginRight: 10
+	},
+	opKeyText: {
+		color: "white"
+	}
+});
--- components/NumPad.js
+++ components/NumPad.js
@@ -1,16 +1,52 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import Key from "./Key";
 
 export default function NumPad(props) {
 	return (
 		<View style={styles.numpad}>
-			<Text>Keys will show up here</Text>
+			<View style={styles.numgroup}>
+				<Key symbol="1" />
+				<Key symbol="2" />
+				<Key symbol="3" />
+				<Key symbol="4" />
+			</View>
+			<View style={styles.numgroup}>
+				<Key symbol="5" />
+				<Key symbol="6" />
+				<Key symbol="7" />
+				<Key symbol="8" />
+			</View>
+			<View style={styles.numgroup}>
+				<Key symbol="9" />
+				<Key symbol="0" />
+				<Key symbol="." />
+				<Key symbol="=" />
+			</View>
+			<ScrollView horizontal={true}>
+				<Key symbol="DEL" op={true} />
+				<Key symbol="+" op={true} />
+				<Key symbol="-" op={true} />
+				<Key symbol="*" op={true} />
+				<Key symbol="/" op={true} />
+				<Key symbol="(" op={true} />
+				<Key symbol=")" op={true} />
+				<Key symbol="sin" op={true} />
+				<Key symbol="cos" op={true} />
+				<Key symbol="tan" op={true} />
+			</ScrollView>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	numpad: {
-		flex: 2
+		flex: 2,
+		padding: 20
+	},
+	numgroup: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "space-between"
 	}
 });
